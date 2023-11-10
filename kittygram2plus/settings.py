c1374d97126c4fdea65aa5a2ebf0a69749ea58ea
Denis Hrushev <super.hrushev2@mail.ru>
2023-11-10 14:20:00 +0300

    Throttling scope

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -136,6 +136,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.thorttling.UserRateThrottling',
+        'rest_framework.thorttling.ScopedRateThrottle',
         # Не будем подключать класс AnonRateThrottle глобально.
         # Подключим его только в тех view-классах или вьюсетах,
         # где надо установить лимиты для анонимов
@@ -144,6 +145,9 @@ REST_FRAMEWORK = {
         # Но сами лимиты установим, и они будут доступны из всего кода проекта
         'user': '10000/day',  # Лимит для UserRateThrottle
         'anon': '1000/day',  # Лимит для AnonRateThrottle
+        # Имена (ключи) для scope придумывает разработчик,
+        # в меру собственной фантазии
+        'low_request': '1/minute',
     }
 }
 
